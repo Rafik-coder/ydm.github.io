82994b54c542fe81b94f73c8ed1f50c5842877ff
xCoder <codewithraf769@gmail.com>
2022-08-10 09:05:46 +0000

    third change

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,18 @@
+
+// ACTIVITIES
+
+function hide(here){
+    let f = document.getElementById(`${here}`)
+    f.classList.replace("show", "hide")
+}
+
+function show(here){
+    let f = document.getElementById(`${here}`)
+    f.classList.replace("hide", "show")
+}
+
+
+// let fort = document.querySelector("#invent")
+// fort.addEventListener("click", () => {
+//     console.log("hello")
+// })
